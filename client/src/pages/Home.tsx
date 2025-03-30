ad0978fe9dd3af1475738387904f2df4dfd43ae0
niraj-gautam <nirajgautamith@gmail.com>
2025-03-30 14:21:12 +0545

    minor fix: component import fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import Hero from '../home/Hero';
-import ResearchAreas from '../home/ResearchAreas';
-import FeaturedPublication from '../home/FeaturedPublication';
-import Events from '../home/Events';
-import LatestNews from '../home/LatestNews';
+import Hero from "../components/home/Hero";
+import ResearchAreas from "../components/home/ResearchAreas";
+import FeaturedPublication from "../components/home/FeaturedPublication";
+import Events from "../components/home/Events";
+import LatestNews from "../components/home/LatestNews";
 
 const Home = () => {
-  return (
-    <>
-      <Hero />
-      <ResearchAreas />
-      <FeaturedPublication />
-      <Events />
-      <LatestNews />
-    </>
-  );
+    return (
+        <>
+            <Hero />
+            <ResearchAreas />
+            <FeaturedPublication />
+            <Events />
+            <LatestNews />
+        </>
+    );
 };
 
 export default Home;
